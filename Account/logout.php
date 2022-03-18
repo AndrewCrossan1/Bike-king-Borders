6826af2e28ef51ed100fc92aaf8078b0da44ea10
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-03-18 18:58:51 +0000

    Fixed minor navigation issue

--- Account/logout.php
+++ Account/logout.php
@@ -3,7 +3,7 @@
 
     //Set page name for required content in functions.php (Avoids file navigation errors which are extremely annoying - PHP just be smarter :,( )
     //Also keys is in with setting the pages active in header.php (Very fancy)
-    $PageName = "Logout";
+    $PageName = "Account";
 
     //Set page title for meta-data in header.php (An isset is used in the meta-data to check for this - Overkill because if it's not set I am dumb.)
     $PageTitle = "Logout";
