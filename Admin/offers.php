42cfff05879b26f3bdc8919e919baf6661fd0667
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-03-18 18:58:33 +0000

    Fixed error with active navigation page

--- Admin/offers.php
+++ Admin/offers.php
@@ -1,12 +1,12 @@
 <?php
     //Page Description: Allows the admin to manage current and historical offers
 
-    //Require the header of the page (Includes Navigation, meta-data, etc.)
-    require('../Scripts/header.php');
-
     //Set page name for required content in functions.php (Avoids file navigation errors which are extremely annoying - PHP just be smarter :,( )
     //Also keys is in with setting the pages active in header.php (Very fancy)
     $PageName = "AOffers";
 
     //Set page title for meta-data in header.php (An isset is used in the meta-data to check for this - Overkill because if it's not set I am dumb.)
     $PageTitle = "Manage Offers";
+
+    //Require the header of the page (Includes Navigation, meta-data, etc.)
+    require('../Scripts/header.php');
