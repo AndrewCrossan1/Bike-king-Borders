b12de24ba6fcf102fc7f9e203f137084adfc6f55
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-03-21 17:37:01 +0000

    Login form added (Currently useless but style is created)

--- Account/login.php
+++ Account/login.php
@@ -10,3 +10,106 @@
 
     //Require the header of the page (Includes Navigation, meta-data, etc.)
     require('../Scripts/header.php');
+
+    ?>
+
+<body>
+    <div class="container-md mt-5 bg-dark text-light border rounded">
+        <div class="row p-2">
+            <div class="col-lg text-center py-2">
+                <p class="display-5">Bike King Borders</p>
+                <p class="small">Bike King Borders offers exclusive perks<br/>and discounts for members through email, text and postage (If opted in)</p>
+                <div class="container p-2">
+                    <div class="row p-1 justify-content-center">
+                        <div class="col-3 w-25">
+                            <img class="img-thumbnail" alt="Smiling on bike" src="/Media/Images/smilingonbike15.jpg"/>
+                        </div>
+                        <div class="col-3 w-25">
+                            <img class="img-thumbnail" alt="Smiling on bike" id="loginimg" src="/Media/Images/smilingonbike2.jpg"/>
+                        </div>
+                        <div class="col-3 w-25">
+                            <img class="img-thumbnail" alt="Smiling on bike" id="loginimg" src="/Media/Images/smilingonbike3.jpg"/>
+                        </div>
+                    </div>
+                    <div class="row p-1 justify-content-center">
+                        <div class="col-3 w-25">
+                            <img class="img-thumbnail" alt="Smiling on bike" id="loginimg" src="/Media/Images/smilingonbike4.jpg"/>
+                        </div>
+                         <div class="col-3 w-25">
+                             <img class="img-thumbnail" alt="Smiling on bike" id="loginimg" src="/Media/Images/smilingonbike3.jpg"/>
+                        </div>
+                        <div class="col-3 w-25">
+                            <img class="img-thumbnail" alt="Smiling on bike" id="loginimg" src="/Media/Images/smilingonbike111.jpg"/>
+                        </div>
+                    </div>
+                    <div class="row p-1 justify-content-center">
+                        <div class="col-3 w-25">
+                            <img class="img-thumbnail" alt="Smiling on bike" id="loginimg" src="/Media/Images/smilingonbike7.jpg"/>
+                        </div>
+                        <div class="col-3 w-25">
+                            <img class="img-thumbnail" alt="Smiling on bike" id="loginimg" src="/Media/Images/smilingonbike8.jpg"/>
+                        </div>
+                        <div class="col-3 w-25">
+                            <img class="img-thumbnail" alt="Smiling on bike" id="loginimg" src="/Media/Images/smilingonbike9.jpg"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg bg-light border border-dark rounded text-dark py-2" style="box-shadow: 0px 0px 12px 1px #A2A3A5;">
+                <p class="display-6 text-center">
+                    Login to your account
+                </p>
+                <p class="small p-2 text-center">
+                    Fill out the form to login in order to access member-exclusive offers and discounts
+                </p>
+                <form class="needs-validation mt-auto mb-auto" novalidate method="post">
+                    <div class="form-row text-center">
+                        <div class="col-md">
+                            <div class="form-outline mb-4 p-2">
+                                <label for="Username" class="form-label">Username: </label><br>
+                                <input type="text" id="Username" name="Username" minlength="8" class="form-control w-75 m-auto" placeholder="Username"  required/>
+                                <div class="invalid-feedback">
+                                    Oops! Something doesn't look right
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md">
+                            <div class="form-outline mb-4 p-2">
+                                <label for="Password" class="form-label">Password: </label><br>
+                                <input type="password" id="Password" name="Password" minlength="12" class="form-control w-75 m-auto" placeholder="Password" required/>
+                                <div class="invalid-feedback">
+                                    Oops! Something doesn't look right
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md p-2">
+                                <input type="submit" class="btn btn-primary w-25">
+                                <input type="reset" class="btn btn-secondary w-25">
+                        </div>
+                    </div>
+                    <script>
+                        // Example starter JavaScript for disabling form submissions if there are invalid fields
+                        (function() {
+                            'use strict';
+                            window.addEventListener('load', function() {
+                                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                                var forms = document.getElementsByClassName('needs-validation');
+                                // Loop over them and prevent submission
+                                var validation = Array.prototype.filter.call(forms, function(form) {
+                                    form.addEventListener('submit', function(event) {
+                                        if (form.checkValidity() === false) {
+                                            event.preventDefault();
+                                            event.stopPropagation();
+                                        }
+                                        form.classList.add('was-validated');
+                                    }, false);
+                                });
+                            }, false);
+                        })();
+                    </script>
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
