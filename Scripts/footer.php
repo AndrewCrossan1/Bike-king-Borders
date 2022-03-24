838978ef2eec4dff0d77944218f78f612591f5f5
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-03-24 00:05:26 +0000

    Created footer with necessary links and social media links

--- Scripts/footer.php
+++ Scripts/footer.php
@@ -1,1 +1,32 @@
-<?php
+<div class="container-fluid bg-dark p-3 text-light" style="margin-top: 6%;">
+        <section id="footer" class="text-center">
+            <div class="row justify-content-center">
+                <div class="col-sm-2 m-3">
+                    Bike King Borders&copy<br>
+                    Near Hadrian's Wall<br>
+                    07363234654<br>
+                    contact@bikekingborders.scot
+                </div>
+                <div class="col-sm-2 m-3">
+                    <!--Facebook-->
+                    <a href="https://facebook.com/"><i class="fa fa-facebook" aria-hidden="false" style="font-size: 40px; color:#3b5998;"></i></a>
+                    <p>Facebook</p>
+                </div>
+                <div class="col-sm-2 m-3">
+                    <!--WhatsApp-->
+                    <a href="https://whatsapp.com/"><i class="fab fa-whatsapp" aria-hidden="false" style="font-size: 40px;color: #25D366;"></i></a>
+                    <p>WhatsApp</p>
+                </div>
+                <div class="col-sm-2 m-3">
+                    <!--Instagram-->
+                    <a href="https://instagram.com/"><i class="fa fa-instagram" aria-hidden="false" style="font-size: 40px;"></i></a>
+                    <p>Instagram</p>
+                </div>
+                <div class="col-sm-2 m-3">
+                    Andrew Crossan<br>
+                    Software & Web Developer<br>
+                    andrew.crossan23@outlook.com<br>
+                    <a href="https://andrewcrossan1.github.io">https://andrewcrossan1.github.io</a>
+                </div>
+        </section>
+    </div>
