b56e0d2fc84576a98db233e90d727387e90dba71
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-04-06 19:03:11 +0000

    Error401 added

---
+++ Errors/Error401.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+require_once('../Scripts/header.php')
+?>
+
+<div class="container-md mt-5 text-center">
+    <h1 class="display-2" style="font-family: Ubuntu, Verdana;">Bike King Borders</h1>
+    <i class="fas fa-triangle-exclamation" style="font-size: 75px;"></i>
+    <h1 class="display-6">Administrator Username or Password incorrect</h1>
+    <p class="lead">Click <a href="/index.php">here</a> to go to the homepage</p>
+</div>
