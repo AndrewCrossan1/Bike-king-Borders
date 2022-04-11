56c7feea789230643acd708fc14a3bbe6be07a6e
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-04-11 21:17:32 +0000

    Administrator home added

---
+++ Admin/home.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+//Page Description: Allows the admin to manage current and historical offers
+
+//Set page name for required content in functions.php (Avoids file navigation errors which are extremely annoying - PHP just be smarter :,( )
+//Also keys is in with setting the pages active in header.php (Very fancy)
+$PageName = "AdminHome";
+
+//Set page title for meta-data in header.php (An isset is used in the meta-data to check for this - Overkill because if it's not set I am dumb.)
+$PageTitle = "Admin Home";
+
+//Require the header of the page (Includes Navigation, meta-data, etc.)
+require('../Scripts/adminheader.php');
+
+//If user is not admin then leave
+if (!isset($_SESSION['Admin'])) {
+    ?>
+    <script>
+        setTimeout(function(){
+            window.location.href = 'index.php';
+        });
+    </script>
+    <?php
+}
+?>
