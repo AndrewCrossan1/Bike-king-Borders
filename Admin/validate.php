ff09ea42eef6ba0c7febe2347ccec8700fc429bd
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-04-11 20:52:29 +0100

    Added login script, currently some form of error but will be fixed shortly

--- Admin/validate.php
+++ Admin/validate.php
@@ -1,4 +1,54 @@
 <?php
+    session_start();
+    //Include functions
+    require_once('../Scripts/functions.php');
 
-
-?>
+    if (isset($_SESSION['Admin'])) {
+        ?>
+        <!--Redirect to admin home page-->
+        <script>
+            setTimeout(function() {
+                window.location.href = 'products.php';
+            },10);
+        </script>
+        <?php
+    } else {
+        if (isset($_POST['submit'])) {
+            if (AdminFunctions::Login($_POST['Username'], $_POST['Password'])) {
+                //Set username
+                $_SESSION['AdminUsername'] = $_POST['Username'];
+                //Retrieve user details
+                $User = AdminFunctions::GetDetails($_SESSION['AdminUsername']);
+                //Set message to be displayed to user upon logging in
+                $Message = base64_encode("Welcome ${$_POST['Username']}");
+                ?>
+                <script>
+                    setTimeout(function() {
+                        window.location.href = 'index.php?message=<?php echo $Message; ?>';
+                    },10)
+                </script>
+                <?php
+            } else {
+                $Message = base64_encode("Invalid username or password");
+                ?>
+                <!--Redirect to admin login page with message saying invalid login-->
+                <script>
+                    setTimeout(function() {
+                       window.location.href = 'login.php?message=<?php echo $Message; ?>';
+                    },10);
+                </script>
+                <?php
+            }
+        } else {
+            $Message = base64_encode("Invalid Username or password");
+            ?>
+            <!--Redirect to admin login page with message saying invalid login-->
+            <script>
+                setTimeout(function() {
+                    window.location.href = 'login.php?message=<?php echo $Message; ?>';
+                },10);
+            </script>
+            <?php
+        }
+    }
+    ?>
