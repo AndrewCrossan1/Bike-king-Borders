26191cb6bceca5473323ac8192cfcb45b7080afe
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-04-19 08:43:32 +0000

    Added top header and basic placeholders for now

--- Admin/offers.php
+++ Admin/offers.php
@@ -29,3 +29,52 @@ if(!isset($_SESSION['Admin']) && !$_SESSION['Admin'] == 1) {
         </div>
     </div>
 </section>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-2 col-12 bg-dark">
+            <!--Side navigation bar-->
+            <div class="container-fluid p-5">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4 class="text-light">Bike King Borders</h4>
+                        <hr style="color: white;"/>
+                        <div class="nav flex-column nav-pills">
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="/admin/home/">Home</a>
+                            </li>
+                            <hr style="color: white;"/>
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="/admin/products/">Products</a>
+                            </li>
+                            <hr style="color: white;"/>
+                            <li class="nav-item">
+                                <a class="nav-link text-light active" aria-selected="true" href="/admin/offers/">Offers</a>
+                            </li>
+                            <hr style="color: white;"/>
+                            <li class="nav-item">
+                                <a class="nav-link text-light" href="/admin/users/" tabindex="-1">Users</a>
+                            </li>
+                            <hr style="color: white;"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-10 col-12">
+            <!--Main content-->
+            <div class="container-fluid p-3">
+                <div class="row justify-content-center">
+                    <div class="col-md-4 col-12 text-center border">
+                        <p class="lead fs-4 p-1">Newly added products</p>
+                    </div>
+                    <div class="col-md-4 col-12 text-center border">
+                        <p class="lead fs-4 p-1">Newly added users</p>
+                    </div>
+                    <div class="col-md-4 col-12 text-center border">
+                        <p class="lead fs-4 p-1">Newly added offers</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
