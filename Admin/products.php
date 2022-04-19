d548c0ce493fa63a4a517cb287a7f51722c4974c
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-04-19 20:28:05 +0100

    Products now displayed from database, products currently can only be deleted, message showing also added

--- Admin/products.php
+++ Admin/products.php
@@ -11,7 +11,7 @@
 
     //Require the header of the page (Includes Navigation, meta-data, etc.)
     require('../Scripts/adminheader.php');
-error_reporting(0);
+    require_once('../Scripts/functions.php');
 if(!isset($_SESSION['Admin']) && !$_SESSION['Admin'] == 1) {
     ?>
     <script>
@@ -62,16 +62,55 @@ if(!isset($_SESSION['Admin']) && !$_SESSION['Admin'] == 1) {
         </div>
         <div class="col-md-10 col-12">
             <!--Main content-->
+            <?php
+            if (isset($_REQUEST['message'])) {
+                functions::SendMessage(base64_decode($_REQUEST['message']));
+            }
+            ?>
             <div class="container-fluid p-3">
                 <div class="row justify-content-center">
-                    <div class="col-md-4 col-12 text-center border">
-                        <p class="lead fs-4 p-1">Newly added products</p>
-                    </div>
-                    <div class="col-md-4 col-12 text-center border">
-                        <p class="lead fs-4 p-1">Newly added users</p>
-                    </div>
-                    <div class="col-md-4 col-12 text-center border">
-                        <p class="lead fs-4 p-1">Newly added offers</p>
+                    <div class="col-md-12 col-12">
+                        <div class="card bg-white border border-primary p-3 my-2">
+                            <div class="card-body">
+                                <table class="table table-striped table-hover table-responsive">
+                                    <thead class="bg-dark text-white text-left">
+                                    <th>ProductID</th>
+                                    <th>Name</th>
+                                    <th>Description</th>
+                                    <th>Price</th>
+                                    <th>Colour</th>
+                                    <th>Age</th>
+                                    <th>Type</th>
+                                    <th>Actions</th>
+                                    </thead>
+                                    <tbody>
+                                    <?php
+                                    $array = AdminFunctions::GetProducts(25);
+                                    while ($row = $array->fetch_assoc()) {?>
+                                        <tr>
+                                            <td><?php echo $row['ProductID']; ?></td>
+                                            <td><?php echo $row['Name']; ?></td>
+                                            <td><?php echo $row['Description']; ?></td>
+                                            <td><?php echo $row['Price']; ?></td>
+                                            <td><?php echo $row['Colour']; ?></td>
+                                            <td><?php echo $row['Age']; ?></td>
+                                            <td><?php echo $row['Type']; ?></td>
+                                            <td>
+                                                <a href="/admin/product/edit/id=<?php echo $row['ProductID'];?>"><i class="fa fa-pencil text-dark"></i></a>
+                                                <a href="/admin/product/delete/id=<?php echo $row['ProductID'];?>"><i class="fa fa-trash-can text-dark"></i></a>
+                                                <a href="/product/view/id=<?php echo $row['ProductID'];?>"><i class="fa fa-magnifying-glass text-dark"></i></a>
+                                            </td>
+                                        </tr>
+                                    <?php } ?>
+                                    </tbody>
+                                    <tfoot class="text-center bg-dark">
+                                        <tr>
+                                            <td colspan="100"><a class="btn btn-outline-light w-100 p-3 m-0" href="/admin/product/add/">Add Product</a></td>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
