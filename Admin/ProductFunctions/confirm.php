593d781e24c59fb57104735ce1f43ce3c52be6a8
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-04-26 00:17:41 +0000

    Added confirmation for editing a product, works and fully functional

--- Admin/ProductFunctions/confirm.php
+++ Admin/ProductFunctions/confirm.php
@@ -135,3 +135,86 @@ if (isset($_POST['AddSubmit'])) {
         <?php
     }
 }
+if (isset($_POST['SaveSubmit'])) {
+    //Setting keys
+    $cd = array(
+        "Name" => null,
+        "Description" => null,
+        "imgslug" => null,
+        "Price" => null,
+        "Colour" => null,
+        "Age" => null,
+        "Type" => null,
+    );
+    //Cleaning Variables
+    $Name = filter_input(INPUT_POST, "ProductName", FILTER_SANITIZE_STRING);
+    $Price = filter_input(INPUT_POST, "ProductPrice", FILTER_VALIDATE_FLOAT);
+    $Type = filter_input(INPUT_POST, "ProductType", FILTER_SANITIZE_STRING);
+    $Colour = filter_input(INPUT_POST, "ProductColour", FILTER_SANITIZE_STRING);
+    $Age = filter_input(INPUT_POST, "ProductAge", FILTER_VALIDATE_INT);
+    $cd['Name'] = $Name;
+    $cd['Price'] = $Price;
+    $cd['Colour'] = $Colour;
+    $cd['Age'] = $Age;
+    $cd['Type'] = $Type;
+    //Description and File are both nullable
+    if (isset($_POST['ProductDescription'])) {
+        $cd['Description'] = filter_input(INPUT_POST, "ProductDescription", FILTER_SANITIZE_STRING);
+    }
+    if (isset($_FILES['ProductImage'])) {
+        //Check if file can be saved
+        $cd['imgslug'] = $_FILES['ProductImage']['name'];
+    }
+    //Proceed to database upload then file saving
+    $Database = new Database();
+    $sql = "UPDATE products SET Name = ?, Price = ?, Type = ?, Colour = ?, Age = ?, Description = ?, imgslug = ? WHERE ProductID = ?;";
+    if ($query = $Database->conn->prepare($sql)) {
+        //Array with keys is used for readability
+        if ($query->bind_param("sdssissi", $cd['Name'], $cd['Price'], $cd['Type'], $cd['Colour'], $cd['Age'], $cd['Description'], $cd['imgslug'], $_REQUEST['id'])) {
+            if ($query->execute()) {
+                if ($cd["imgslug"] != null) {
+                    //Get temporary save location
+                    $TempFileName = $_FILES['ProductImage']['tmp_name'];
+                    //Set target directory
+                    $target_dir = "../../Media/Products/" . $_REQUEST['id'] . "/";
+                    //Set error message for common if statements
+                    $Message = base64_encode("Could not modify product");
+                    if (!move_uploaded_file($TempFileName, $target_dir . $cd['imgslug'])) {
+                        ?>
+                        <script>
+                            window.location.href = "https://localhost/admin/home/?message=<?php echo $Message; ?>";
+                        </script>
+                        <?php
+                    } else {
+                        ?>
+                        <script>
+                            window.location.href = "https://localhost/admin/home/?message=<?php echo $Message; ?>";
+                        </script>
+                        <?php
+                    }
+                }
+                $Message = base64_encode("Product successfully modified");
+                ?>
+                <script>
+                    window.location.href = "https://localhost/admin/home/?message=<?php echo $Message; ?>";
+                </script>
+                <?php
+            } else {
+            }
+        } else {
+            $Message = base64_encode("Could not modify product!");
+            ?>
+            <script>
+                window.location.href = "https://localhost/admin/home/?message=<?php echo $Message; ?>";
+            </script>
+            <?php
+        }
+    } else {
+        $Message = base64_encode("Could not modify product!");
+        ?>
+        <script>
+            window.location.href = "https://localhost/admin/home/?message=<?php echo $Message; ?>";
+        </script>
+        <?php
+    }
+}
