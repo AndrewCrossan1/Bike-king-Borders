8c5939233499942f6e7860de1197f5d473ed1ad7
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-04-28 23:35:37 +0000

    Classes are now auto loaded, alot of similar changes have been made

--- Account/account.php
+++ Account/account.php
@@ -1,16 +1,13 @@
 <?php
 session_start();
-//Page Description: Allows the user to view information on their account, also change it if they wish!
-if (isset($_SESSION['Username'])) {
-    //Set page name for required content in functions.php (Avoids file navigation errors which are extremely annoying - PHP just be smarter :,( )
-    //Also keys is in with setting the pages active in header.php (Very fancy)
-    $PageName = "Account";
 
+if (isset($_SESSION['Username'])) {
     //Set page title for meta-data in header.php (An isset is used in the meta-data to check for this - Overkill because if it's not set I am dumb.)
     $PageTitle = "My Account";
 
     //Require the header of the page (Includes Navigation, meta-data, etc.)
-    require('../Scripts/header.php');
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/' . 'settings.php');
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/Scripts/' . 'functions.php');
 } else {
     ?>
     <script>
