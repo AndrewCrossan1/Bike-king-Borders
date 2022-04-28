c7a1321570c4c5937a8558c9801ead945ae62ed6
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-04-28 23:41:36 +0000

    settings.php now loads relevant classes for account, products and normal pages

---
+++ settings.php
@@ -0,0 +1,18 @@
+<?php
+
+//Defining base directory
+define("BASE_DIR", $_SERVER['DOCUMENT_ROOT'] . "/");
+
+//Scripts
+include_once(BASE_DIR .'Scripts/database.php');
+include_once($_SERVER['DOCUMENT_ROOT'] . '/Scripts/' . 'functions.php');
+
+//Models
+include_once(BASE_DIR .'Models/Account.php');
+include_once(BASE_DIR .'Models/Customer.php');
+include_once(BASE_DIR .'Models/Category.php');
+include_once(BASE_DIR .'Models/Product.php');
+include_once(BASE_DIR .'Models/Basket.php');
+
+include(BASE_DIR .'Scripts/header.php');
+?>
