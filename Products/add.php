46ab463cda458ece5644d152224458873733aa39
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-04-30 21:22:03 +0000

    Minor changes to accomodate new url

--- Products/add.php
+++ Products/add.php
@@ -7,7 +7,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/' . 'settings.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/Scripts/' . 'functions.php');
 
 //Check if basket is set
-if (!isset($_GET['ProductID'])){
+if (!isset($_GET['id'])){
     ?>
     <script>
         setTimeout(function() {
@@ -17,7 +17,7 @@ if (!isset($_GET['ProductID'])){
     <?php
 } else {
     $db = new Database();
-    $result = $db->Select("SELECT * FROM products WHERE ProductID = ?", array((int)$_GET['ProductID']));
+    $result = $db->Select("SELECT * FROM products WHERE ProductID = ?", array((int)$_GET['id']));
     $Product = $result->fetch_assoc();
     $NewProduct = new Product($Product['ProductID'], $Product['Name'], $Product['Description'], $Product['Price'], $Product['imgslug'], $Product['Colour'], $Product['Age'], $Product['Type']);
     $_SESSION['basket']->Add($NewProduct, 1);
