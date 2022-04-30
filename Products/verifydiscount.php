f20bb2973a556ee87924b146cbc0d308df4e7a76
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-04-30 23:54:07 +0000

    Users can now apply any valid discount codes

---
+++ Products/verifydiscount.php
@@ -0,0 +1,52 @@
+<?php
+require($_SERVER['DOCUMENT_ROOT'] . "/Scripts/" . "database.php");
+if (isset($_GET['discountcode'])) {
+    $Database = new Database();
+    //Get corresponding code from database
+    $sql = "SELECT * FROM Offers WHERE Code = ? & ValidTo <= sysdate()";
+    $query = $Database->conn->prepare($sql);
+    if ($query->bind_param("s", $_GET['discountcode'])) {
+        //Execute
+        if ($query->execute()) {
+            $result = $query->get_result();
+            if ($result->num_rows == 1) {
+                $row = $result->fetch_assoc();
+                $discount = $row['Discount'];
+                $Message = base64_encode("Discount applied!");
+                ?>
+                <script>
+                    window.location.href="/basket/?discountmessage=<?php echo $Message; ?>&discount=<?php echo (float)$discount;?>&code=<?php echo $_GET['discountcode'];?>";
+                </script>
+                <?php
+            } else {
+                $Message = base64_encode("Invalid code!");
+                ?>
+                <script>
+                    window.location.href="/basket/?discountmessage=<?php echo $Message; ?>";
+                </script>
+                <?php
+            }
+        } else {
+            $Message = base64_encode("Invalid code!");
+            ?>
+            <script>
+                window.location.href="/basket/?discountmessage=<?php echo $Message; ?>";
+            </script>
+            <?php
+        }
+    } else {
+        $Message = base64_encode("Invalid code!");
+        ?>
+        <script>
+            window.location.href="/basket/?discountmessage=<?php echo $Message; ?>";
+        </script>
+        <?php
+    }
+} else {
+    ?>
+    <script>
+        window.location.href="/basket/";
+    </script>
+    <?php
+}
+?>
