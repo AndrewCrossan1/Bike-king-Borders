fc02709e6852585f30492f2fef8eeb0a78b4da7a
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-05-10 16:01:25 +0100

    Tickets are now created and saved in database, also viewable on admin site

--- Contact/ContactSend.php
+++ Contact/ContactSend.php
@@ -1,1 +1,40 @@
 <?php
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/Scripts/database.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/settings.php');
+
+if (!isset($_REQUEST['ContactSubmit'])) {
+    ?>
+    <script>
+        window.location.href = '/contact/';
+    </script>
+    <?php
+}
+
+//SQL Statement
+$sql = "INSERT INTO Tickets (Subject, Message, Email, Fullname, DateCreated, ProductID) VALUES (?, ?, ?, ?, sysdate(), ?)";
+
+//Create new database object
+$Database = new Database();
+
+//Get product from POST
+$Product = $_REQUEST['Product'];
+//Get the ProductID using the name of the bike
+$ProductQuery = $Database->Select("SELECT ProductID FROM products WHERE Name = ?", array($Product));
+//Bind result to variable
+$ProductID = (int)$ProductQuery->fetch_assoc()['ProductID'];
+
+//Insert
+$Message = base64_encode("Ticket could not be created");
+if ($query = $Database->conn->prepare($sql)) {
+    if ($query->bind_param("ssssi", $_REQUEST['Subject'], $_REQUEST['Message'], $_REQUEST['Email'], $_REQUEST['FullName'], $ProductID)) {
+        if ($query->execute()) {
+            $Message = base64_encode("Support Ticket created");
+        }
+    }
+}
+?>
+<script>
+    window.location.href = '/Contact/?Message=<?php echo $Message;?>';
+</script>
+
