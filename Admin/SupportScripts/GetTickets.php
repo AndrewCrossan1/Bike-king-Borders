a43191f05adedf84a20cbaf9cf1ee9fcf89729f9
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-05-10 12:00:57 +0100

    Checked if result is null, then return 'No Products Found'

--- Admin/SupportScripts/GetTickets.php
+++ Admin/SupportScripts/GetTickets.php
@@ -11,9 +11,9 @@ $DatePosted = $_REQUEST['dateposted'];
 
 //Set SQL statement
 $sql = match ($Status) {
-    'All' => "SELECT tickets.TicketID, tickets.Email, tickets.Fullname, tickets.Subject, tickets.Message, tickets.DateCreated, tickets.Active, products.ProductID, products.Name FROM tickets, products WHERE tickets.ProductID = products.ProductID",
-    'Closed' => "SELECT tickets.TicketID, tickets.Email, tickets.Fullname, tickets.Subject, tickets.Message, tickets.DateCreated, tickets.Active, products.ProductID, products.Name FROM tickets, products WHERE tickets.ProductID = products.ProductID AND Active = 0",
-    'Active' => "SELECT tickets.TicketID, tickets.Email, tickets.Fullname, tickets.Subject, tickets.Message, tickets.DateCreated, tickets.Active, products.ProductID, products.Name FROM tickets, products WHERE tickets.ProductID = products.ProductID AND Active = 1",
+    'All' => "SELECT Tickets.TicketID, Tickets.Email, Tickets.Fullname, Tickets.Subject, Tickets.Message, Tickets.DateCreated, Tickets.Active, products.ProductID, products.Name FROM Tickets, products WHERE Tickets.ProductID = products.ProductID",
+    'Closed' => "SELECT Tickets.TicketID, Tickets.Email, Tickets.Fullname, Tickets.Subject, Tickets.Message, Tickets.DateCreated, Tickets.Active, products.ProductID, products.Name FROM Tickets, products WHERE Tickets.ProductID = products.ProductID AND Active = 0",
+    'Active' => "SELECT Tickets.TicketID, Tickets.Email, Tickets.Fullname, Tickets.Subject, Tickets.Message, Tickets.DateCreated, Tickets.Active, products.ProductID, products.Name FROM Tickets, products WHERE Tickets.ProductID = products.ProductID AND Active = 1",
 };
 
 //Append ordering to sql statement
@@ -25,8 +25,11 @@ $sql = match ($DatePosted) {
 $array = array();
 
 $Tickets = $Database->Select($sql);
-
-while ($result = $Tickets->fetch_assoc()) {
-    array_push($array, $result);
+if ($Tickets == null) {
+    echo "No products found";
+} else {
+    while ($result = $Tickets->fetch_assoc()) {
+        array_push($array, $result);
+    }
+    echo json_encode($array);
 }
-echo json_encode($array);
