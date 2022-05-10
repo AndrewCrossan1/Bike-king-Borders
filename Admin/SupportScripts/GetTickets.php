c8a09e62aca24dab8853b2854551e1674453e63b
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-05-10 15:17:13 +0100

    AJAX Filtering removed due to taking too long, not ideal for time left to complete project, filtering works though.

--- Admin/SupportScripts/GetTickets.php
+++ Admin/SupportScripts/GetTickets.php
@@ -1,4 +1,6 @@
 <?php
+
+session_start();
 //Require necessary
 require ($_SERVER['DOCUMENT_ROOT'] . "/Scripts/database.php");
 
@@ -11,25 +13,36 @@ $DatePosted = $_REQUEST['dateposted'];
 
 //Set SQL statement
 $sql = match ($Status) {
-    'All' => "SELECT Tickets.TicketID, Tickets.Email, Tickets.Fullname, Tickets.Subject, Tickets.Message, Tickets.DateCreated, Tickets.Active, products.ProductID, products.Name FROM Tickets, products WHERE Tickets.ProductID = products.ProductID",
-    'Closed' => "SELECT Tickets.TicketID, Tickets.Email, Tickets.Fullname, Tickets.Subject, Tickets.Message, Tickets.DateCreated, Tickets.Active, products.ProductID, products.Name FROM Tickets, products WHERE Tickets.ProductID = products.ProductID AND Active = 0",
-    'Active' => "SELECT Tickets.TicketID, Tickets.Email, Tickets.Fullname, Tickets.Subject, Tickets.Message, Tickets.DateCreated, Tickets.Active, products.ProductID, products.Name FROM Tickets, products WHERE Tickets.ProductID = products.ProductID AND Active = 1",
+    'all' => "SELECT Tickets.TicketID, Tickets.Email, Tickets.Fullname, Tickets.Subject, Tickets.Message, Tickets.DateCreated, Tickets.Active, products.ProductID, products.Name FROM Tickets, products WHERE Tickets.ProductID = products.ProductID",
+    'closed' => "SELECT Tickets.TicketID, Tickets.Email, Tickets.Fullname, Tickets.Subject, Tickets.Message, Tickets.DateCreated, Tickets.Active, products.ProductID, products.Name FROM Tickets, products WHERE Tickets.ProductID = products.ProductID AND Active = 0",
+    'active' => "SELECT Tickets.TicketID, Tickets.Email, Tickets.Fullname, Tickets.Subject, Tickets.Message, Tickets.DateCreated, Tickets.Active, products.ProductID, products.Name FROM Tickets, products WHERE Tickets.ProductID = products.ProductID AND Active = 1",
 };
 
 //Append ordering to sql statement
 $sql = match ($DatePosted) {
-    'Newest' => $sql . ' ORDER BY TicketID DESC;',
-    'Oldest' => $sql . ' ORDER BY TicketID ASC;',
+    'newest' => $sql . ' ORDER BY TicketID DESC;',
+    'oldest' => $sql . ' ORDER BY TicketID ASC;',
 };
 
 $array = array();
 
 $Tickets = $Database->Select($sql);
 if ($Tickets == null) {
-    echo "No products found";
+    $_SESSION['filteredcontent'] = null;
+    ?>
+    <script>
+        window.location.href = '/admin/support/status=<?php echo $Status;?>/order=<?php echo $DatePosted;?>/';
+    </script>
+    <?php
 } else {
     while ($result = $Tickets->fetch_assoc()) {
         array_push($array, $result);
     }
-    echo json_encode($array);
+    $_SESSION['filteredcontent'] = $array;
+    ?>
+    <script>
+        window.location.href = '/admin/support/status=<?php echo $Status;?>/order=<?php echo $DatePosted;?>/';
+    </script>
+    <?php
 }
+?>
