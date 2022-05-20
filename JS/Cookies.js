e741aea1b75fd6346802f606390218a7fe7af4e5
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-05-21 00:39:58 +0100

    Cookies can now be retrieved, will be used for displaying viewed images on homepage

--- JS/Cookies.js
+++ JS/Cookies.js
@@ -1,17 +1,34 @@
-function SetCookies() {
-    let date = Date.now();
-    let today = new Date(date);
-    document.cookie = `VisitingTime=${today.toUTCString()}`;
+function GetCookie(CookieName) {
+    let cookies = document.cookie.split(';');
+    for (let x = 0; x<cookies.length; x++) {
+        let c = ca[i];
+        while (c.charAt(0) == " ") {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(CookieName.length, c.length);
+        }
+    }
+    return null;
+}
 
-    /* Add "https://api.ipify.org?format=json" statement
-              this will communicate with the ipify servers in
-              order to retrieve the IP address $.getJSON will
-              load JSON-encoded data from the server using a
-              GET HTTP request */
-    $.getJSON("https://api.ipify.org?format=json", function(data) {
-        document.cookie = `IP=${data.ip}`
-    });
-};
+function SetCookies() {
+    if (GetCookie("VisitingTime=") == null) {
+        let date = Date.now();
+        let today = new Date(date);
+        document.cookie = `VisitingTime=${today.toUTCString()}`;
+    }
+    if (GetCookie("IP=") == null) {
+        /* Add "https://api.ipify.org?format=json" statement
+                 this will communicate with the ipify servers in
+                 order to retrieve the IP address $.getJSON will
+                 load JSON-encoded data from the server using a
+                 GET HTTP request */
+        $.getJSON("https://api.ipify.org?format=json", function(data) {
+            document.cookie = `IP=${data.ip}`
+        });
+    }
+}
 
 function SetClickedPhotoURL(URL, ImageID) {
     //Check if item is already set
