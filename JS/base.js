e37065c868ee60f5b3d6c7cc0bb53ce3e3e5f3db
AndrewCrossan1 <andrew.crossan23@outlook.com>
2022-05-24 23:47:10 +0100

    Added keybindings for navigating the website

--- JS/base.js
+++ JS/base.js
@@ -2,4 +2,39 @@ $(document).ready(function() {
     $("#PriceRange").change(function() {
         document.getElementById('PriceRangeLabel').innerText = `Price Range: £0 - £${$(this).val()}`;
     });
+    $(document).keydown(function(event) {
+        //Decide what to do based on key pressed
+        if (event.ctrlKey) {
+            event.stopPropagation();
+            event.preventDefault();
+            switch (event.key) {
+                case 'h':
+                    window.location.href = "/home/";
+                    break;
+                case 'p':
+                    window.location.href = '/products/';
+                    break;
+                case 'b':
+                    window.location.href = '/basket/';
+                    break;
+                case 'l':
+                    window.location.href = '/localarea/';
+                    break;
+                case 'a':
+                    window.location.href = '/account/login/';
+                    break;
+                case 'c':
+                    window.location.href = '/contact/';
+                    break;
+                case 'g':
+                    window.location.href = '/gallery/';
+                    break;
+                case 'm':
+                    window.location.href = '/admin/';
+                    break;
+            }
+        }
+    });
 });
+
+
